e615c0ba93c29f758e58604b253e6bba285bd0a9
rubnsbarbosa <rubensbarbosa90@gmail.com>
2019-07-10 14:05:13 -0300

    entrada de dados (input)

---
+++ BasicPython/inputPython.py
@@ -0,0 +1,8 @@
+'''
+Entrada de dados em python
+'''
+name = input('Qual o seu nome? ')
+idade = input('Qual a sua idade')
+
+print(f'Bem vindo(a), {name} e tem {idade} anos.')
+print(f'O tipo da variável é {type(name)}')
