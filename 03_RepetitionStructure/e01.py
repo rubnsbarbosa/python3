7525c820ca0ea234875c1d2fd6da9b559fbc2571
rubnsbarbosa <rubensbarbosa90@gmail.com>
2019-10-02 11:41:43 -0300

    repetition

---
+++ 03_RepetitionStructure/e01.py
@@ -0,0 +1,4 @@
+nota = float(input('Digite uma nota entra 0 e 10: '))
+while(nota > 10 or nota < 0):
+    print('Valor da nota esta invalido!')
+    nota = float(input('Digite uma nota entra 0 e 10: '))
